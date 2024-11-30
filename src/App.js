6e9a294983417eeda821abfa43401964a716796b
Afiqu3 <79781916+Afiqu3@users.noreply.github.com>
2024-11-30 14:00:16 +0600

    No Refactor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [noteTitle, setNoteTitle] = useState("");
+  const [notes, setNotes] = useState([]);
+  const [updateMode, setUpdateMode] = useState(false);
+  const [editableNote, setEditableNote] = useState(null);
+
+  const createHandler = (event) => {
+    event.preventDefault();
+    if (!noteTitle) {
+      return alert("Enter note title");
+    }
+    const newNote = {
+      id: Date.now() + "",
+      title: noteTitle,
+    };
+    setNotes([...notes, newNote]);
+    setNoteTitle("");
+  };
+
+  const removeHandler = (noteId) => {
+    const newNotes = notes.filter((note) => note.id !== noteId);
+    setNotes(newNotes);
+  };
+
+  const editHandler = (note) => {
+    setUpdateMode(true);
+    setNoteTitle(note.title);
+    setEditableNote(note);
+  };
+
+  const updateHandle = (event) => {
+    event.preventDefault();
+    const updateNotes = notes.map((note) => {
+      if (note.id === editableNote.id) {
+        return {
+          ...note,
+          title: noteTitle,
+        };
+      }
+      return note;
+    });
+    setNotes(updateNotes);
+    setUpdateMode(false);
+    setEditableNote(null);
+    setNoteTitle("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={updateMode ? updateHandle : createHandler}>
+        <input
+          type="text"
+          value={noteTitle}
+          onChange={(event) => setNoteTitle(event.target.value)}
+        />
+        <button type="submit">{updateMode ? "Update Note" : "Add Note"}</button>
+      </form>
+      <div>
+        <ul>
+          {notes.map((note) => (
+            <li key={note.id}>
+              <span>{note.title}</span>
+              <button onClick={() => editHandler(note)}>Edit</button>
+              <button onClick={() => removeHandler(note.id)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
